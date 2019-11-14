714c337276333f2f60416f623497a6bb6541220a
vbouillennec <val.bouillennec@gmail.com>
2019-11-14 15:45:00 +0100

    API: +GameAPI & search parameter in GET request

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 const FetchData = (props) => {
 
-	const [data, setData] = useState();
+	const [data, setData] = useState([]);
 
 	useEffect(() => {
 
 		const fetchString = async () => {
-			const req = await fetch('sampleAPI');
+			const req = await fetch('GameAPI');
 			const localdata = await req.json();
-
 			setData(localdata);
 		}
 
@@ -20,10 +19,50 @@ const FetchData = (props) => {
 		}
 	}, []);
 
+	console.log(data);
+	if(data.length > 0)
+	{
+		console.log(Object.keys(data[1]));
+	}
+
+	function showDataTable(data){
+		if(data.length > 0){
+			const keys = Object.keys(data[0]);
+			return (
+				<table class="table table-striped" styles={{width: 100}}>
+					<thead>
+						<tr>
+						{
+							keys.map((col, key) => <th key={key}>{col}</th>)
+						}
+						</tr>
+					</thead>
+					<tbody>
+					{
+						data.map((row, key) => {
+							return (
+							<tr key={key}>
+								<td>{row.logiciel.uri}</td>
+								<td>{row.nom.value}</td>
+								<td>{row.wiki.uri}</td>
+								{/* <td>{row.photo ? row.photo.uri : ''}</td> */}
+								<td>{row.resume.value}</td>
+							</tr>
+							)
+						})
+					}
+					</tbody>
+				</table>
+			)
+			
+		}
+	}
+
 	return (
 		<React.Fragment>
 			<p>Hello</p>
-			<p>Fetched Data from c# backend: {data}</p>
+			<p>Fetched Data from c# backend: </p>
+			{showDataTable(data)}
 		</React.Fragment>
 	)
 }
