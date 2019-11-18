c7400c3901193b953437de6bccdc520384149bdb
vbouillennec <val.bouillennec@gmail.com>
2019-11-18 17:45:06 +0100

    API: GamesAPI & GameAPI

--- Controllers/GameAPI.cs
+++ Controllers/GameAPI.cs
@@ -8,6 +8,7 @@ using VDS.RDF.Query;
 using VDS.RDF;
 using System.Data;
 using Newtonsoft.Json;
+using VDS.RDF.Parsing;
 
 namespace cs_rdf.Controllers
 {
@@ -19,9 +20,9 @@ namespace cs_rdf.Controllers
 				[HttpGet]
 				public string Get()
 				{
-					string q = HttpContext.Request.Query["q"].ToString();
+					string game = HttpContext.Request.Query["game"].ToString();
 
-					return getDataFromEndpoint(q);
+					return getDataFromEndpoint(game);
 					// return Enumerable.Range(1, 1).Select(index => new String("Hello world !!")).ToArray();
 				}
 
@@ -30,24 +31,51 @@ namespace cs_rdf.Controllers
 				// {
 				// }
 
-				private string getDataFromEndpoint(string q){
+				private string getDataFromEndpoint(string game){
 					TripleStore store = new TripleStore();
 
 					SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
-					SparqlResultSet results = endpoint.QueryWithResultSet("SELECT ?logiciel ?nom ?wiki ?photo ?resume WHERE { ?logiciel a dbo:Software ; dbo:abstract ?resume ; rdfs:label ?nom ; foaf:isPrimaryTopicOf ?wiki OPTIONAL { ?logiciel dbo:thumbnail ?photo } FILTER langMatches(lang(?resume), 'fr') FILTER langMatches(lang(?nom), 'fr') FILTER regex(?nom, \"^"+q+"\") } LIMIT 50");
+					
+					//First we need an instance of the SparqlQueryParser
+					SparqlQueryParser parser = new SparqlQueryParser();
+
+					//Then we build the query string
+
+					//Then we can parse a SPARQL string into a query
+					SparqlQuery query = parser.ParseFromString("SELECT * WHERE { ?s a ?type }");
+
+					string queryString = @"SELECT DISTINCT *
+					WHERE {
+						dbr:"+game+@" rdfs:label ?Jeu
+						OPTIONAL { dbr:"+game+@" dbo:thumbnail ?Photo }
+						OPTIONAL { dbr:"+game+@" dbo:genre ?Genre }
+						OPTIONAL { dbr:"+game+@" dbo:developer ?Developpeur }
+						OPTIONAL { dbr:"+game+@" dbo:series ?Series }
+						OPTIONAL { dbr:"+game+@" dbo:producer ?Producteur }
+						OPTIONAL { dbr:"+game+@" dbo:computingPlatform ?Plateforme }
+						OPTIONAL { dbr:"+game+@" dbo:releaseDate ?Date_de_sortie }
+						OPTIONAL { dbr:"+game+@" dbo:director ?Realisateur }
+						OPTIONAL { dbr:"+game+@" dbp:modes ?Modes_de_jeu }
+						OPTIONAL { dbr:"+game+@" dbo:abstract ?Resume }
+						OPTIONAL { dbr:"+game+@" foaf:isPrimaryTopicOf ?Wiki }
+						FILTER langMatches(lang(?Resume), 'fr')
+						FILTER langMatches(lang(?Jeu), 'fr')
+					} LIMIT 100";
+
+					SparqlResultSet results = endpoint.QueryWithResultSet(queryString);
 					// SparqlResultSet results = endpoint.QueryWithResultSet("SELECT * WHERE { ?logiciel a dbo:Software } LIMIT 20");
 					// string stringRes = "";
 					List<Dictionary<string, INode>> listResult = new List<Dictionary<string, INode>>();
-					if(results is SparqlResultSet)
+					if(results is SparqlResultSet && results.Any())
 					{
 						SparqlResultSet rset = (SparqlResultSet) results;
 						int nbRes = 0;
 						IEnumerator<KeyValuePair<string, INode>> columns;
 						KeyValuePair<string, INode> col;
+						nbRes = rset.Count();
+						Console.WriteLine(nbRes+" entries found");
 						foreach(SparqlResult line in rset)
 						{
-							nbRes = line.Count();
-							Console.WriteLine(nbRes);
 							columns = line.GetEnumerator();
 							Dictionary<string, INode> arrayLine = new Dictionary<string, INode>();
 							while(columns.MoveNext()) {
@@ -70,28 +98,32 @@ namespace cs_rdf.Controllers
 								// listResult.Add(line.First().Key + " => " + line.First().Value);
 							// }
 						}
+						string json = JsonConvert.SerializeObject(listResult, Formatting.Indented);
+						return json;
 					}
 					// listResult.Get();
-
-					string json = JsonConvert.SerializeObject(listResult, Formatting.Indented);
-
-					return json;
+					Console.WriteLine("Invalid entry ! Please enter a correct game");
+					return "Invalid entry ! Please enter a correct game";
 				}
 		}
 }
 
 /*
-SELECT ?logiciel ?nom ?wiki ?photo ?resume 
-WHERE { 
-	?logiciel a dbo:Software ; 
-		dbo:abstract ?resume ; 
-		rdfs:label ?nom ; 
-		foaf:isPrimaryTopicOf ?wiki
-	OPTIONAL {
-		?logiciel dbo:thumbnail ?photo }
-	FILTER langMatches(lang(?resume), 'fr') 
-	FILTER langMatches(lang(?nom), 'fr') 
-	FILTER regex(?nom, "^Dark") 
-} 
-LIMIT 20
+SELECT DISTINCT *
+WHERE {
+dbr:Warcraft rdfs:label ?Jeu
+OPTIONAL { dbr:Warcraft dbo:thumbnail ?Photo }
+OPTIONAL { dbr:Warcraft dbo:genre ?Genre }
+OPTIONAL { dbr:Warcraft dbo:developer ?Developpeur }
+OPTIONAL { dbr:Warcraft dbo:series ?Series }
+OPTIONAL { dbr:Warcraft dbo:producer ?Producteur }
+OPTIONAL { dbr:Warcraft dbo:computingPlatform ?Plateforme }
+OPTIONAL { dbr:Warcraft dbo:releaseDate ?Date_de_sortie }
+OPTIONAL { dbr:Warcraft dbo:director ?Realisateur }
+OPTIONAL { dbr:Warcraft dbp:modes ?Modes_de_jeu }
+OPTIONAL { dbr:Warcraft dbo:abstract ?Resume }
+OPTIONAL { dbr:Warcraft foaf:isPrimaryTopicOf ?Wiki }
+FILTER langMatches(lang(?Resume), 'fr')
+FILTER langMatches(lang(?Jeu), 'fr')
+} LIMIT 100
 */
