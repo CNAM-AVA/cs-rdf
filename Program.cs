349d2f6052b741a8d0faec934821e7a72b6c97e7
antoine plard <azertyzhd@gmail.com>
2019-11-23 11:18:49 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace cs_rdf
         public static void Main(string[] args)
         {
             SparqlQueryParser parser = new SparqlQueryParser();
-            SparqlQuery q = parser.ParseFromString("SELECT ?logiciel ?nom ?wiki ?photo ?resume WHERE { ?logiciel a dbo:Software ; dbo:abstract ?resume ; rdfs:label ?nom ; foaf:isPrimaryTopicOf ?wiki ; dbo:thumbnail ?photo FILTER langMatches(lang(?resume), 'fr') FILTER langMatches(lang(?nom), 'fr') FILTER regex(?nom, \"^Dark\") } LIMIT 20");
+            //SparqlQuery q = parser.ParseFromString("SELECT ?logiciel ?nom ?wiki ?photo ?resume WHERE { ?logiciel a dbo:Software ; dbo:abstract ?resume ; rdfs:label ?nom ; foaf:isPrimaryTopicOf ?wiki ; dbo:thumbnail ?photo FILTER langMatches(lang(?resume), 'fr') FILTER langMatches(lang(?nom), 'fr') FILTER regex(?nom, \"^Dark\") } LIMIT 20");
             // Console.WriteLine(q.ToString());
             CreateWebHostBuilder(args).Build().Run();
         }
